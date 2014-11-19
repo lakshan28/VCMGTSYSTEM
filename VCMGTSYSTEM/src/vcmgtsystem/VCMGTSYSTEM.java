38d4fcfad8c568fb21a9fb8a5e5eb2288c444904
chathura666 <chathuradulanga@gmail.com>
2014-11-19 17:52:46 +0530

    wttff

--- VCMGTSYSTEM/src/vcmgtsystem/VCMGTSYSTEM.java
+++ VCMGTSYSTEM/src/vcmgtsystem/VCMGTSYSTEM.java
@@ -17,6 +17,7 @@ public class VCMGTSYSTEM {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("dfdfgfd");
     }
     
 }
